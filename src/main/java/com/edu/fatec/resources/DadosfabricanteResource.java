ae0d85e0e61ea8c2c0a6b44ae866da07c6bf23c4
unknown <beneditotiqlz@gmail.com>
2019-11-19 01:02:56 -0300

    classe de dominio

--- src/main/java/com/edu/fatec/resources/DadosfabricanteResource.java
+++ src/main/java/com/edu/fatec/resources/DadosfabricanteResource.java
@@ -1,14 +1,22 @@
 package com.edu.fatec.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.edu.fatec.domain.Dadosfabricante;
 
 @RestController
 @RequestMapping(value="/dadosfabricante")
 public class DadosfabricanteResource {
 	@RequestMapping(method=RequestMethod.GET)
-public String listar () {
-	return "Fabricante esta funcionando";
+public List<Dadosfabricante> listar () {
+	Dadosfabricante fab = new Dadosfabricante(1,"ford", null);
+	List<Dadosfabricante> lista = new ArrayList<>();
+	lista.add (fab);	
+	return lista;
 }
 }
