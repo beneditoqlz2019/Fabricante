2fc875106022ed658ba6f6ce73097cead498e31e
unknown <beneditotiqlz@gmail.com>
2019-11-19 00:00:11 -0300

    Projeto Criado

---
+++ src/test/java/com/edu/fatec/FabricanteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edu.fatec;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FabricanteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
